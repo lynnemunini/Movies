a046f0fb4901bd111dc13686c2dabb6c28c30916
Lynne Munini <muninilynne65@gmail.com>
2022-01-09 13:21:55 -0500

    Fixed rating  and review feature bug

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, FloatField, IntegerField
 from wtforms.validators import DataRequired
 import requests
+from pprint import pprint
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
@@ -15,17 +16,21 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///movies.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+movies_endpoint = "https://api.themoviedb.org/3/search/movie"
+API_KEY = "2b4011c8374dc66a46f4c80fe3cda32d"
+MOVIE_DB_IMAGE_URL = "https://image.tmdb.org/t/p/w500"
+
 
 ##CREATE TABLE
 class Movies(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(250), unique=True, nullable=False)
     year = db.Column(db.Integer, nullable=False)
-    description = db.Column(db.String, nullable=False)
-    rating = db.Column(db.Float, nullable=False)
-    ranking = db.Column(db.Integer, nullable=False)
-    review = db.Column(db.String, nullable=False)
-    image_url = db.Column(db.String, nullable=False)
+    description = db.Column(db.String(500), nullable=False)
+    rating = db.Column(db.Float, nullable=True)
+    ranking = db.Column(db.Integer, nullable=True)
+    review = db.Column(db.String(250), nullable=True)
+    image_url = db.Column(db.String(250), nullable=False)
 
     #Optional: this will allow each Movie object to be identified by its title when printed.
     def __repr__(self):
@@ -46,7 +51,7 @@ class UpdateForm(FlaskForm):
 
 #CREATE ADD FORM
 class AddForm(FlaskForm):
-    add = StringField('Movie Title', validators=[DataRequired()])
+    title = StringField('Movie Title', validators=[DataRequired()])
     submit = SubmitField('Add Movie')
 
 
@@ -66,8 +71,8 @@ def edit():
         movie_rating = request.form["rating"]
         movie_review = request.form["review"]
         movie_to_update = Movies.query.get(movie_id)
-        movie_to_update.rating = movie_rating
-        movie_to_update.review = movie_review
+        movie_to_update.rating = movie_rating if movie_to_update.rating is None else movie_rating
+        movie_to_update.review = movie_review if movie_to_update.review is None else movie_review
         db.session.commit()
         return redirect(url_for("home"))
 
@@ -83,10 +88,46 @@ def delete():
     db.session.commit()
     return redirect(url_for('home'))
 
-@app.route("/add")
+@app.route("/add", methods=["GET", "POST"])
 def add():
     form = AddForm()
+    if form.validate_on_submit():
+        movie_title = request.form["title"]
+        query = {
+            "api_key": API_KEY,
+            "language": "en-US",
+            "query": movie_title,
+            "include_adult": "false"
+        }
+        response = requests.get(url=movies_endpoint, params=query)
+        results =  response.json()["results"]
+        # pprint(results)
+        return render_template('select.html', movies=results)
+
     return render_template('add.html', form=form)
+
+
+@app.route("/movie")
+def get_movie():
+    movie_id = request.args.get('id')
+    url_endpoint = "https://api.themoviedb.org/3/movie"
+    query = {
+        "api_key": API_KEY,
+        "language": "en-US"
+        }
+    response = requests.get(url=f"{url_endpoint}/{movie_id}", params=query)
+    # pprint(response.json())
+    results = response.json()
+    title = results["title"]
+    year = int(results["release_date"].split("-")[0])
+    description = results["overview"]
+    img_url=f"{MOVIE_DB_IMAGE_URL}{results['poster_path']}"
+    movie_data = Movies(title=title, year=year, description=description, image_url=img_url)   
+    db.session.add(movie_data)
+    db.create_all()    
+    db.session.commit()
+    return redirect(url_for('edit'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
